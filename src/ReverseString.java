1bba2292caeb71a491ba80f3de08cafaa848be0d
Zayniddin <tuit0778@gmail.com>
2024-11-19 16:01:14 +0500

    reverse string

---
+++ src/ReverseString.java
@@ -0,0 +1,16 @@
+public class ReverseString {
+
+    public static void main(String[] args) {
+        reverseString(new char[]{'h', 'e', 'l', 'l', 'o' });
+    }
+
+    public static void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i++] = s[j];
+            s[j--] = temp;
+        }
+    }
+}
