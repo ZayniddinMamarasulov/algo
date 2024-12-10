847e135d2f02e6a351c84d5c094ac0e3d24ef604
Zayniddin <tuit0778@gmail.com>
2024-12-10 15:54:21 +0500

    MaxTwoEvents2054

---
+++ src/MaximumDepthOfBinaryTree104.java
@@ -0,0 +1,9 @@
+
+public class MaximumDepthOfBinaryTree104 {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
