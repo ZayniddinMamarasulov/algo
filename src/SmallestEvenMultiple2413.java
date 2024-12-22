661950952339eca6a777a69786c701345eb9fe3d
Zayniddin <tuit0778@gmail.com>
2024-12-22 18:16:39 +0500

    SmallestEvenMultiple2413

---
+++ src/SmallestEvenMultiple2413.java
@@ -0,0 +1,11 @@
+public class SmallestEvenMultiple2413 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) return n;
+        return n * 2;
+    }
+}
