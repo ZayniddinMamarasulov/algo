a612e24767679fd71209d2af5f98b7f9399efe0e
Zayniddin <tuit0778@gmail.com>
2024-12-23 10:37:58 +0500

    FibonacciNumber509 improvement

--- src/FibonacciNumber509.java
+++ src/FibonacciNumber509.java
@@ -5,8 +5,7 @@ public class FibonacciNumber509 {
     }
 
     public int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+        if (n == 0 || n == 1) return n;
         return fib(n) + fib(n - 1);
     }
 
