dac5bf4168d5936e84ac2b23a46fab62e0342397
Zayniddin <tuit0778@gmail.com>
2024-12-24 10:59:53 +0500

    NumberOfGoodPairs1512

---
+++ src/NumberOfGoodPairs1512.java
@@ -0,0 +1,19 @@
+public class NumberOfGoodPairs1512 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (i == j) continue;
+                if (nums[i] == nums[j]) count++;
+            }
+        }
+
+        return count;
+    }
+}
