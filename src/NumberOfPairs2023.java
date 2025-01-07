8ad72fde03a8e94f43e2df3bfa13bfda47694362
Zayniddin <tuit0778@gmail.com>
2025-01-07 12:26:17 +0500

    NumberOfPairs2023.java

---
+++ src/NumberOfPairs2023.java
@@ -0,0 +1,26 @@
+public class NumberOfPairs2023 {
+
+    public static void main(String[] args) {
+//        numOfPairs(new String[]{"777", "7", "77", "77"}, "7777");
+        numOfPairs(new String[]{"123", "4", "12", "34"}, "1234");
+//        numOfPairs(new String[]{"1", "1", "1"}, "11");
+    }
+
+    public static int numOfPairs(String[] nums, String target) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            String temp = target.replaceFirst(nums[i], "");
+            int tempIndex = target.indexOf(nums[i]);
+
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j)
+                    if (nums[j].equals(temp) && tempIndex == 0) {
+                        count++;
+                    }
+            }
+        }
+
+        return count;
+    }
+}
