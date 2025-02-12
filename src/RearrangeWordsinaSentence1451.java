4b84e42644b4c454507ea15953e3241c9d1c3f59
Zayniddin <tuit0778@gmail.com>
2025-02-12 15:15:14 +0500

    RearrangeWordsinaSentence1451.java

--- src/RearrangeWordsinaSentence1451.java
+++ src/RearrangeWordsinaSentence1451.java
@@ -3,27 +3,59 @@ import java.util.*;
 public class RearrangeWordsinaSentence1451 {
 
     public static void main(String[] args) {
-
+//        arrangeWords("  this   is  a sentence ");
+        arrangeWords(" practice   makes   perfect");
     }
 
-    public String arrangeWords(String text) {
+    public static String arrangeWords(String text) {
+
+        StringBuilder[] builderArray = new StringBuilder[100];
+        int k = 0;
+        int count = 0;
         StringBuilder builder = new StringBuilder();
 
-        String[] text1 = text.split(" ");
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == 32) {
+                if (!builder.isEmpty()) {
+                    builderArray[k++] = builder;
+                    builder = new StringBuilder();
+                }
+                count++;
+            } else {
+                builder.append(text.charAt(i));
+            }
+        }
+        if (!builder.isEmpty()) builderArray[k++] = builder;
 
-        Arrays.sort(text1, Comparator.comparingInt(String::length));
+        StringBuilder resBuilder = new StringBuilder();
 
-        for (int i = 0; i < text1.length; i++) {
-            String cur = text1[i];
-            String res = i == 0
-                    ? (cur.substring(0, 1).toUpperCase() + cur.substring(1, cur.length() - 1).toLowerCase())
-                    : cur.toLowerCase();
+        int spaceLen = 0;
+        int spaceRemain = 0;
 
-            builder.append(res);
-            if (i != text1.length - 1) builder.append(" ");
+        if (k != 1) {
+            spaceLen = count / (k - 1);
+            spaceRemain = count % (k - 1);
+        } else {
+            spaceRemain = count;
         }
 
 
-        return builder.toString();
+        for (int i = 0; i < k; i++) {
+            int space = spaceLen;
+            resBuilder.append(builderArray[i]);
+
+            if (i != k - 1)
+                while (space > 0) {
+                    resBuilder.append(" ");
+                    space--;
+                }
+        }
+
+        while (spaceRemain > 0) {
+            resBuilder.append(" ");
+            spaceRemain--;
+        }
+
+        return resBuilder.toString();
     }
 }
